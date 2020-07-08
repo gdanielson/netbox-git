07c772e4e7a0bdd3815586a4d1871cd5770f3ec3
Graeme Danielson <graeme.danielson@netspan.co.nz>
2020-07-08 16:29:24 +1200

    Testing setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(
     author="gdanielson",
     license="MIT",
     packages=["netboxgit"],
+    install_requires=[
+        "certifi==2020.4.5.2",
+        "chardet==3.0.4",
+        "gitdb==4.0.5",
+        "GitPython==3.1.3",
+        "idna==2.9",
+        "pynetbox==4.3.1",
+        "requests==2.24.0",
+        "six==1.15.0",
+        "smmap==3.0.4",
+        "urllib3==1.25.9",
+    ],
     zip_safe=False,
 )
