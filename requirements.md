a2b6d91e8f953054382aa2cc124cd7f8a1a12cb5
Graeme Danielson <graeme.danielson@netspan.co.nz>
2020-07-08 14:39:45 +1200

    Package installer updates & rename example scripts
    New file setup.py for python-packaging
    Rename runner.py -> example.py
    Rename backout_example.py -> example_backout.py

--- requirements.md
+++ requirements.md
@@ -1,2 +1,3 @@
 GitPython>=3.1.3
+
 pynetbox>=4.3.1
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="netboxgit",
+    version="0.1",
+    description="Manage NetBox object data into Git version control.",
+    url="https://github.com/gdanielson/netbox-git",
+    author="gdanielson",
+    license="MIT",
+    packages=["netboxgit"],
+    zip_safe=False,
+)
